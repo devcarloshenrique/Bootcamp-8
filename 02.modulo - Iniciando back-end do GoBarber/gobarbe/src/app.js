5f7bdd1bc6babdc94d5df427af577a8e9ed881d0
Carlos Henrique Carvalho de Araujo <dig.CarlosHenrique@opovo.local>
2020-01-23 15:41:20 -0300

    refactor: refactoring imports with sucrase

--- 02.modulo - Iniciando back-end do GoBarber/gobarbe/src/app.js
+++ 02.modulo - Iniciando back-end do GoBarber/gobarbe/src/app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const routes = require('./routes');
+import express from 'express';
+import routes from './routes';
 
 class App {
   
@@ -28,4 +28,4 @@ class App {
 }
 
 // O único atributo ou metodo que pode ser acessado é o server;
-module.exports = new App().server;
+export default new App().server;
