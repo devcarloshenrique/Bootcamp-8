985505d2ddc8c51562afaf03518e2346fb7e17c8
devcarloshenrique <devcarloshenrique@gmail.com>
2020-02-11 17:51:39 -0300

    refactor: new authentication and update routes

--- 02.modulo - Iniciando back-end do GoBarber/gobarbe/src/routes.js
+++ 02.modulo - Iniciando back-end do GoBarber/gobarbe/src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddllewares from './app/middllewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+// Rota Global de verificação JWT, só vai ocorrer verificações apartir desta rota.
+routes.use(authMiddllewares);
+routes.put('/users', UserController.update);
 
 export default routes;
