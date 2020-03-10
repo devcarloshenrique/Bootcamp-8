8ac7419ce3c014ab097904ec2b0c4bbfaca79b39
devcarloshenrique <devcarloshenrique@gmail.com>
2020-03-10 12:03:15 -0300

    feat: configuration provided sequelize

---
+++ 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/config/database.js
@@ -0,0 +1,14 @@
+// Este arquivo não pode a sitaxe do sucrese pois vai ser acessado tanto pela aplicação quanto pelo sequelize
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'admin',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    // Quero utilizar o padrão de tabelas e colunas underscored
+    underscored: true,
+    underscoredAll: true,
+  },
+};
