e9be451e44bb45fd794173aaadfb28a0e94d3aa2
devcarloshenrique <devcarloshenrique@gmail.com>
2020-03-30 08:57:38 -0300

    feat: previous settings

--- 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/.eslintrc.js
+++ 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/.eslintrc.js
@@ -15,22 +15,22 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
-  },  
+  },
   rules: {
 
     "prettier/prettier":  "error",
-
+ 
   // No eslint por padrão, todo metodo de uma class precisa usar this.
     "class-methods-use-this":  "off",
-
+ 
   // No eslint por padrão, não é possível receber parametros e fazer alterações
     "no-param-reassign" :  "off",
-
+ 
   // Por padrão o eslint não permite que variáveis sejá criada desta manéira variavel_impossible
     "camelcase":  "off",
-
+ 
   // Por padrão o eslint não permite declarar uma variável e não utiliza-la Ele vai ignorar a variavel next, caso eu chame ela como parametro e não use-a.
-
-    "no-unused-vars": ["error", { "argsIgnorePattern":  "next" }],
+ 
+    "no-unused-vars": ["error", { "argsIgnorePattern":  "next" }]
   },
 };
