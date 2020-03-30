8e5a16c3c20b57a7d7c648e19468ad54145167ba
devcarloshenrique <devcarloshenrique@gmail.com>
2020-03-30 08:59:05 -0300

    refactor: route create users

--- 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/routes.js
+++ 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/routes.js
@@ -1,8 +1,13 @@
 // Separa a forma de roteamento do express em outro arquivo.
 import { Router } from 'express';
 
+import User from './app/models/User';
+
+// Importando o metodo store do controller user que é responsável por criar os user
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hellow World' }));
+routes.post('/users', UserController.store);
 
 export default routes;
