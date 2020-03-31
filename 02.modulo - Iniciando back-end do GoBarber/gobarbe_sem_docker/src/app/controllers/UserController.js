ac375492bb2700aa2b135b094f35b8d4f07e4c1d
devcarloshenrique <devcarloshenrique@gmail.com>
2020-03-31 13:24:11 -0300

    feat: implementation of the update method

--- 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/app/controllers/UserController.js
+++ 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/app/controllers/UserController.js
@@ -20,6 +20,39 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    // o req.userId é passado por meio do token e definido no middlewares/auth.js
+    const user = await User.findByPk(req.userId);
+
+    // verificando se o email é igual ao email do banco
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ message: 'User already exists' });
+      }
+    }
+
+    // Se oldPassword for true, chama a função que verifica se oldPassword == Password_hash(db)
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    // Atualizando os dados do banco de acordo com os dados do req.body
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
 }
 
 export default new UserController();
