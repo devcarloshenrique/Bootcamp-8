e87bef8c5323931c44059ad9253339fea22f324a
devcarloshenrique <devcarloshenrique@gmail.com>
2020-04-25 19:20:30 -0300

    refactor: calling associated method

--- 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/database/index.js
+++ 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/database/index.js
@@ -1,9 +1,12 @@
 import Sequelize from 'sequelize';
+
 import User from '../app/models/User';
+import File from '../app/models/File';
+
 import databaseConfig from '../config/database';
 
 // Array com todos os models da aplicação até o seguinte momento
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -15,8 +18,11 @@ class Database {
     // Quando este metodo é chamado é necessário importar as config do DB| Conexão pronta.
     this.connection = new Sequelize(databaseConfig);
 
-    // Está sendo chamada a model User, o metodo init, passando como parametro a conexão que o parametro sequelize pede
     models.map(model => model.init(this.connection));
+    // Está sendo chamada a model User, o metodo init, passando como parametro a conexão que o parametro sequelize pede
+    models.map(
+      model => model.associate && model.associate(this.connection.models)
+    );
   }
 }
 
