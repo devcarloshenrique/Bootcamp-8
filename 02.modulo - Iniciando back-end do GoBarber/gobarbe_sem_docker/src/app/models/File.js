52fc8e13a37b2a4948decfd961cfdffe1b9e2b14
devcarloshenrique <devcarloshenrique@gmail.com>
2020-04-26 18:00:51 -0300

    refactor: creating virtual field in file

--- 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/app/models/File.js
+++ 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/app/models/File.js
@@ -9,6 +9,12 @@ class File extends Model {
         // Apenas colunas em que o usuario cadastra dados, não precisam ser reflexo da base de dados
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3333/files/${this.path} `;
+          },
+        },
       },
       {
         // Como segundo parametro pode ser passada outras configurações além de retornar a variavel sequelize
