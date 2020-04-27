16c7e69595856f324c9be8c894fe582b96204d7c
devcarloshenrique <devcarloshenrique@gmail.com>
2020-04-27 17:38:08 -0300

    refactor : calling appointment model

--- 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/database/index.js
+++ 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/database/index.js
@@ -2,11 +2,12 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 import databaseConfig from '../config/database';
 
 // Array com todos os models da aplicação até o seguinte momento
-const models = [User, File];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
