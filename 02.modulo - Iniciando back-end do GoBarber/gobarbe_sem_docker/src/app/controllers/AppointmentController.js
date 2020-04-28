545ae21ea1aecea4b5f425fe440780fb934e442b
devcarloshenrique <devcarloshenrique@gmail.com>
2020-04-28 16:51:07 -0300

    refactor: listing and validating appointment schedules

--- 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/app/controllers/AppointmentController.js
+++ 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/app/controllers/AppointmentController.js
@@ -1,8 +1,34 @@
-import * as Yup from 'Yup';
+import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 
 class AppointmentController {
+  async index(req, res) {
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json({ appointments });
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -16,7 +42,7 @@ class AppointmentController {
     const { provider_id, date } = req.body;
 
     // verificando se o provider_id as provider é igual a true
-    const isProvider = await User.findOne({
+    const CheckIsProvider = await User.findOne({
       where: {
         // Estou procurando um usuario pelo provider_id e filtrando para apenas os usuarios com provider == true
         id: provider_id,
@@ -24,16 +50,42 @@ class AppointmentController {
       },
     });
 
-    if (!isProvider) {
+    if (!CheckIsProvider) {
       return res
         .status(401)
         .json({ error: 'You can only create appointment with provider' });
     }
 
+    /* ParseISO transforma essa date em objeto.date do javascript, este objeto pode ser usado dentro do
+     * metodo startOfHour que vai aredondar as horas 18:35 == 18:00
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    // Verificando se hourStart está antes de new Date()
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    // Checando se tem horario disponivel
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not avaliable' });
+    }
+
+    // Criando Appointment
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
 
     return res.json(appointment);
