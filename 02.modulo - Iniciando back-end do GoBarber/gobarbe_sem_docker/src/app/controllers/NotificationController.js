28eb7b548078ca4ed421a7b10f671c26dfc1b2b3
devcarloshenrique <devcarloshenrique@gmail.com>
2020-05-09 18:18:34 -0300

    refactor: creating the update method in NotificationsController

--- 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/app/controllers/NotificationController.js
+++ 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/app/controllers/NotificationController.js
@@ -24,6 +24,17 @@ class NotificationController {
 
     return res.json(notifications);
   }
+
+  async update(req, res) {
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      // retorna o valor atualizado, apenas se for atualizado.
+      { new: true }
+    );
+
+    return res.json(notification);
+  }
 }
 
 export default new NotificationController();
