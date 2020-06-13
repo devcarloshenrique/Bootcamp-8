f23f68293931ddb067ff510995f58efd951d40f8
devcarloshenrique <devcarloshenrique@gmail.com>
2020-06-13 09:03:14 -0300

    refactor: past and cancelable virtual fields

--- 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/app/models/Appointment.js
+++ 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/app/models/Appointment.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import { isBefore, subHours } from 'date-fns';
 
 class Appointment extends Model {
   // Método chamado auto pelo sequelize
@@ -9,6 +10,24 @@ class Appointment extends Model {
         // Apenas colunas em que o usuario cadastra dados, não precisam ser reflexo da base de dados
         date: Sequelize.DATE,
         canceled_at: Sequelize.DATE,
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            /**
+             * Retorna true se o horário já tiver passado
+             */
+            return isBefore(this.date, new Date());
+          },
+        },
+        cancelable: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            /**
+             * Retorna false se o date for 2 horas menores
+             */
+            return isBefore(new Date(), subHours(this.date, 2));
+          },
+        },
       },
       {
         // Como segundo parametro pode ser passada outras configurações além de retornar a variavel sequelize
