d6cb5f72154871e26f12823c885f9ee865f03213
devcarloshenrique <devcarloshenrique@gmail.com>
2020-06-13 12:50:21 -0300

    refactor: configuring sentry

--- 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/app.js
+++ 02.modulo - Iniciando back-end do GoBarber/gobarbe_sem_docker/src/app.js
@@ -1,5 +1,12 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
+/**
+ * Deve está antes das rotas
+ */
+import 'express-async-errors';
 import routes from './routes';
 
 import './database/index';
@@ -8,12 +15,24 @@ class App {
   constructor() {
     this.server = express();
 
-    this.middlewares();
+    /**
+     * Inicializando sentry
+     */
+    Sentry.init(sentryConfig);
 
+    this.middlewares();
     this.routes();
+    this.exceptionHadler();
   }
 
   middlewares() {
+    /**
+     * Seguindo a documentação do setry é necessário colar este code
+     * antes de qual quer rota ou requisição
+     */
+
+    this.server.use(Sentry.Handlers.requestHandler());
+
     // Para à aplicação conseguir consumir json, é necessário chamar essa função do express
     this.server.use(express.json());
     // Este metodo static faz com que a "url" da imagem seja reconhecida pelo nevegador
@@ -26,6 +45,22 @@ class App {
   // As rotas serão exportadas de routes.js
   routes() {
     this.server.use(routes);
+    /**
+     * Seguindo a documentação do setry é necessário colar este code
+     * depois de todas as rotas
+     */
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  /**
+   * Retornar o devido error, quando uma requisição não responder
+   */
+  exceptionHadler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
